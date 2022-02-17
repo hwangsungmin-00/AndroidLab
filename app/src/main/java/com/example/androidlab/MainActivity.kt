ee4470c769068366a507b6ad16dff067821cb261
hwangsungmin <hwangsungmin_00@naver.com>
2022-02-17 15:46:28 +0900

    ch6.카카오톡 비밀번호 확인 화면

--- app/src/main/java/com/example/androidlab/MainActivity.kt
+++ app/src/main/java/com/example/androidlab/MainActivity.kt
@@ -13,32 +13,6 @@ import androidx.core.content.ContextCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        /*
-        //이름 문자열 출력
-        val name=TextView(this).apply{
-            typeface= Typeface.DEFAULT_BOLD
-            text="Lake Louise"
-        }
-
-        //이미지 출력
-        //val image=ImageView(this).also {
-        //    it.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.lake_1))
-        //}
-
-        //주소 문자열 출력
-        val address=TextView(this).apply{
-            typeface= Typeface.DEFAULT_BOLD
-            text="Lake Louise, AB, 캐나다"
-        }
-        val layout=LinearLayout(this).apply {
-            orientation=LinearLayout.VERTICAL
-            gravity=Gravity.CENTER
-
-            addView(name, WRAP_CONTENT, WRAP_CONTENT)
-            //addView(image, WRAP_CONTENT, WRAP_CONTENT)
-            addView(address, WRAP_CONTENT, WRAP_CONTENT)
-        }
-        */
 
         setContentView(R.layout.activity_main)
 
